7da637e5909d9b0624908078fa57a1c49ed37c52
Platniumfrost <grinningtickingclocker@gmail.com>
2022-10-19 08:45:23 -0600

    final steps

---
+++ Hangman code.py
@@ -0,0 +1,63 @@
+import random
+
+
+HANGMAN=(
+
+    
+"""
+-------
+|   |
+|   O
+| /-+-\\
+|   +
+|  |+|
+|  | |
+|
+|
+|
+|
+|
+|
+|
+----------
+
+""",
+"""
+
+|   |
+|   O
+| /-+-\\
+|   +
+|  |+|
+|  | |
+|
+|
+|
+|
+|
+|
+|
+----------
+""",
+
+    )
+
+MAX_WRONG=len(HANGMAN)-1
+print(HANGMAN[len(HANGMAN)-1])
+
+
+WORDS=("OVERUSE","CLAM","GUAM","TAFFETA")
+##word_index=random.randint(o,len(WORDS))
+##word=WORDS[word_index]
+##print(word_index)
+
+
+word=random.choice(WORDS)#the word to be guessed
+so_far="o"*len(word)#one dash for each letter in word
+used=[]#letters already guessed
+wrong=0#number of wrong guesses player has made
+
+print("Welcome to Hangman.Good luck!")
+print(HANGMAN[0])
+print()
+print(so_far)
